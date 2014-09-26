efe8f6e713c4605c87503d4e0b5c9811c1ac6a98
Eliuk Blau <eliukblau@gmail.com>
2014-09-26 02:42:01 -0300

    .

--- phaser_demo/js/game/demo.js
+++ phaser_demo/js/game/demo.js
@@ -14,7 +14,7 @@
         $extends: Phaser.Game,
 
         initialize: function () {
-            this.$super(800, 600, Phaser.AUTO, "game", null);
+            this.$super(800, 600, Phaser.WEBGL, "game", null);
 
             this.state.add("boot", Demo.State.Boot, false);
             this.state.add("preloader", Demo.State.Preloader, false);
