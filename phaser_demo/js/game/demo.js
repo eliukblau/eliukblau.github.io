5d0677a295b990aa38e80d41eea7f6a96a9262f7
Eliuk Blau <eliukblau@gmail.com>
2014-10-26 03:48:26 -0300

    phaser update
    phaser updated to v2.1.3, and little fix for iOS 8 (Safari)

--- phaser_demo/js/game/demo.js
+++ phaser_demo/js/game/demo.js
@@ -14,19 +14,20 @@
         $extends: Phaser.Game,
 
         initialize: function () {
-            this.$super(800, 600, this.isMobileSafari() ? Phaser.CANVAS : Phaser.AUTO, "game", null);
+            //this.$super(800, 600, this.isMobileSafari() ? Phaser.CANVAS : Phaser.AUTO, "game", null);
+            this.$super(800, 600, Phaser.AUTO, "game", null);
 
             this.state.add("boot", Demo.State.Boot, false);
             this.state.add("preloader", Demo.State.Preloader, false);
             this.state.add("main", Demo.State.Main, false);
 
             this.state.start("boot");
-        },
-
-        isMobileSafari: function () {
-            var UA = window.navigator.userAgent;
-            return UA.match(/(iPod|iPhone|iPad)/) && !UA.match(/CriOS/);
         }
+
+        // isMobileSafari: function () {
+        //     var UA = window.navigator.userAgent;
+        //     return UA.match(/(iPod|iPhone|iPad)/) && !UA.match(/CriOS/);
+        // }
     });
 
 }).call(this);
