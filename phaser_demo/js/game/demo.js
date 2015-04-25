ce073dc7c1fffa534ebb790cb2d53336f07b9493
Eliuk Blau <eliukblau@gmail.com>
2015-04-24 23:41:12 -0300

    Update to Phaser 2.3.0

--- phaser_demo/js/game/demo.js
+++ phaser_demo/js/game/demo.js
@@ -15,7 +15,7 @@
 
         initialize: function () {
             //this.$super(800, 600, this.isMobileSafari() ? Phaser.CANVAS : Phaser.AUTO, "game", null);
-            this.$super(800, 600, Phaser.AUTO, "game", null);
+            this.$super(800, 600, Phaser.WEBGL, "game", null, false, true, null);
 
             this.state.add("boot", Demo.State.Boot, false);
             this.state.add("preloader", Demo.State.Preloader, false);
--- phaser_demo/js/game/state/boot.js
+++ phaser_demo/js/game/state/boot.js
@@ -26,15 +26,22 @@
             this.stage.disableVisibilityChange = true;
 
             // This tells the game to resize the renderer to match the game dimensions (i.e. 100% browser width / height)
-            this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
             this.game.scale.pageAlignHorizontally = true;
             this.game.scale.pageAlignVertically = true;
             this.game.scale.minWidth = 320;
             this.game.scale.minHeight = 240;
             //this.game.scale.maxWidth = 800;
             //this.game.scale.maxHeight = 600;
-            this.game.scale.setScreenSize(true);
-            this.game.scale.refresh();
+            //this.game.scale.refresh();
+
+            this.game.scale.setResizeCallback(function(scale, parentBounds) {
+                var scaleFactor = Math.min(
+                    window.innerWidth / this.game.width,
+                    window.innerHeight / this.game.height
+                );
+                scale.setUserScale(scaleFactor, scaleFactor, 0, 0);
+            }, this);
 
             // iniciamos el preloader
             this.game.state.start("preloader", true, false);
